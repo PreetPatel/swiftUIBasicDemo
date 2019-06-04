78dadc595ae0b0bd0b51e5df9688c26c5bd630b9
Preet Patel <iam@preetpatel.com>
2019-06-04 19:32:43 +1200

    Initial Commit

---
+++ iOSSwiftUIDemo/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  iOSSwiftUIDemo
+//
+//  Created by Preet Patel on 4/06/19.
+//  Copyright © 2019 Preet Patel. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
---
+++ iOSSwiftUIDemoTests/iOSSwiftUIDemoTests.swift
@@ -0,0 +1,34 @@
+//
+//  iOSSwiftUIDemoTests.swift
+//  iOSSwiftUIDemoTests
+//
+//  Created by Preet Patel on 4/06/19.
+//  Copyright © 2019 Preet Patel. All rights reserved.
+//
+
+import XCTest
+@testable import iOSSwiftUIDemo
+
+class iOSSwiftUIDemoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
