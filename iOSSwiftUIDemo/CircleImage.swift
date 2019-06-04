cf3f05738a955170cb62dc8aa8f016d6f2542351
Preet Patel <iam@preetpatel.com>
2019-06-04 21:01:31 +1200

    first commit

---
+++ iOSSwiftUIDemo/CircleImage.swift
@@ -0,0 +1,23 @@
+//
+//  CircleImage.swift
+//  iOSSwiftUIDemo
+//
+//  Created by Preet Patel on 4/06/19.
+//  Copyright © 2019 Preet Patel. All rights reserved.
+//
+
+import SwiftUI
+
+struct CircleImage : View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#if DEBUG
+struct CircleImage_Previews : PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
+#endif
---
+++ iOSSwiftUIDemo/MapView.swift
@@ -0,0 +1,23 @@
+//
+//  MapView.swift
+//  iOSSwiftUIDemo
+//
+//  Created by Preet Patel on 4/06/19.
+//  Copyright © 2019 Preet Patel. All rights reserved.
+//
+
+import SwiftUI
+
+struct MapView : View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#if DEBUG
+struct MapView_Previews : PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
+#endif
